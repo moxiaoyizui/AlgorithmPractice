9b8f1697ff45024763f90d75a608be4a2f9a89e0
yangcq <526555746@qq.com>
2017-11-10 14:28:29 +0800

    主方法

---
+++ src/com/yang/algs/search/Insertion.java
@@ -0,0 +1,10 @@
+package com.yang.algs.search;
+
+import java.util.List;
+
+public class Insertion {
+
+    public void base(int[] origin) {
+
+    }
+}
--- src/com/yang/main/Launcher.java
+++ src/com/yang/main/Launcher.java
@@ -1,8 +1,26 @@
 package com.yang.main;
 
+import java.util.List;
+
 public class Launcher {
     public static void main(String[] args){
-        
+        int[] origin = {1,2,8,1234896,486,456123,456,354,218574,12851,84533,45642313,423185,1260,564045,1546,456,-4231,8108};
+        System.out.println("排序前:");
+        print(origin);
+        System.out.println("\n排序后:");
+        print(origin);
+    }
+
+    private static void print(int[] target) {
+        int counter = 0;
+        for (int num : target) {
+            System.out.print(num);
+            if (counter++ % 10 == 9) {
+                System.out.println("");
+            } else {
+                System.out.print(",");
+            }
+        }
     }
 }
 
