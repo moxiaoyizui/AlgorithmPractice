32e082527f2aec1a9865ce9abb80c454db708017
yangcq <526555746@qq.com>
2017-11-13 17:10:39 +0800

    添加直接插入排序方法

--- src/com/yang/algs/search/Insertion.java
+++ src/com/yang/algs/search/Insertion.java
@@ -7,7 +7,20 @@ package com.yang.algs.search;
  */
 public class Insertion {
 
-    public void base(int[] origin) {
-
+    /**
+     * 直接插入排序
+     * @param origin
+     */
+    public static void straight(int[] origin) {
+        System.out.println("直接插入排序开始...");
+        int size = origin.length;
+        for (int i = 1; i < size;i++) {
+            int j;
+            int tar = origin[i];
+            for (j = i; j >0 && tar < origin[j - 1]; j--) {
+                origin[j] = origin[j - 1];
+            }
+            origin[j] = tar;
+        }
     }
 }
