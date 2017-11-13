db74405895b4fcefd5d850f94e65afcdd787c178
yangcq <526555746@qq.com>
2017-11-13 14:17:43 +0800

    基本冒泡 增强冒泡 双向冒泡

--- src/com/yang/algs/search/Bubble.java
+++ src/com/yang/algs/search/Bubble.java
@@ -7,7 +7,58 @@ package com.yang.algs.search;
  */
 public class Bubble {
 
-    public void base(int[] origin) {
+    public static void base(int[] origin) {
+        System.out.println("基本冒泡排序");
+        int size = origin.length;
+        for (int i = 0; i < size - 1;i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (origin[j] > origin[j + 1]) {
+                    swap(origin, j, j+1);
+                }
+            }
+        }
+    }
 
+    public static void enhanced_laststat(int[] origin) {
+        System.out.println("增强版冒泡排序--完成标记");
+        int size = origin.length;
+        boolean lastStat = false;
+        for (int i = 0; !lastStat && i < size - 1;i++) {
+            lastStat = true;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (origin[j] > origin[j + 1]) {
+                    lastStat = false;
+                    swap(origin, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void enhanced_twoways(int[] origin) {
+        System.out.println("增强版冒泡排序--双向");
+        int size = origin.length;
+        int high = size - 1;
+        int low = 0;
+        while (low < high) {
+            for (int i = low; i < high;i++) {
+                if (origin[i] > origin[i+1]) {
+                    swap(origin, i, i+1);
+                }
+            }
+            high--;
+
+            for (int i = high; i > low;i--) {
+                if (origin[i - 1] > origin[i]) {
+                    swap(origin, i - 1, i);
+                }
+            }
+            low++;
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
